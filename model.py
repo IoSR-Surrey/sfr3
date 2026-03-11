ab83fd97b06bcb84dbe692db4f193900b7114215
Riccardo Passoni <r.passoni@surrey.ac.uk>
2026-03-11 16:27:17 +0000

    automated unet resize

--- model.py
+++ model.py
@@ -6,12 +6,22 @@ import math
 
 # UNet from https://github.com/Janspiry/Image-Super-Resolution-via-Iterative-Refinement
 class SR3UNet(nn.Module):
-    def __init__(self, grid_res=32):
+    def __init__(self, grid_res=32, min_bottleneck=8):
         super().__init__()
         self.grid_res = grid_res
-        # attention at all resolutions from grid_res/2 down to 8
-        attn_res = tuple(grid_res // (2 ** i) for i in range(1, int(math.log2(grid_res)) - 1))
-        self.unet = UNet(in_channel=4, out_channel=2, inner_channel=32, image_size=grid_res, attn_res=attn_res)
+
+        # number of stages so bottleneck spatial size >= min_bottleneck
+        n_stages = int(math.log2(grid_res / min_bottleneck)) + 1  # +1 to account for no final downsample
+        channel_mults = tuple(2 ** i for i in range(n_stages))
+        inner_channel = 64
+        # attention only at resolutions <= 16
+        attn_res = tuple(grid_res // (2 ** i) for i in range(1, n_stages) if grid_res // (2 ** i) <= 16)
+        print(f"n_stages: {n_stages}")
+        print(f"channel_mults: {channel_mults}")
+        print(f"inner_channel: {inner_channel}")
+        print(f"attn_res: {attn_res}")
+        self.unet = UNet(in_channel=4, out_channel=2, inner_channel=inner_channel, channel_mults=channel_mults,
+            image_size=grid_res, attn_res=attn_res)
         self.upsample_input = nn.Upsample(size=(grid_res, grid_res), mode='bicubic', align_corners=False)
 
     def upsample_lr(self, lr_lowres):
